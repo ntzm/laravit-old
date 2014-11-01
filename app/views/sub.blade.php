88e9acb57b39cae507429a5e59fedb94e86dbf16
Nat Zimmermann <nat@natzim.com>
2014-11-01 19:07:07 +0000

    Added post sub-view

--- app/views/sub.blade.php
+++ app/views/sub.blade.php
@@ -4,24 +4,7 @@
   <div class="row">
     <div class="column">
 @foreach ($posts as $post)
-      <div class="panel">
-        <a href="{{ $post->url }}">
-          <h3>{{{ $post->title }}}</h3>
-        </a>
-        <div>
-          <a href="/u/{{ $post->user->name }}">
-            <i class="fa fa-user"></i> /u/{{ $post->user->name }}
-          </a>
-          <a href="/r/{{ $post->sub->name }}">
-            <i class="fa fa-code-fork"></i> /r/{{ $post->sub->name }}
-          </a>
-        </div>
-        <div>
-          <a href="/p/{{ $post->id }}">
-            <i class="fa fa-comments"></i> {{ $post->comments->count() }} comments
-          </a>
-        </div>
-      </div>
+      @include('snippets.post', array('post' => $post))
 @endforeach
       {{ $posts->links('snippets.pagination') }}
     </div>

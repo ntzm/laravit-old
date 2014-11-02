fbe8e410134d03bcb2183fd42cc8fa41523b0c69
Nat Zimmermann <nat@natzim.com>
2014-11-02 21:49:28 +0000

    Updated seeders and migrations (added newline at end of file)

--- app/database/seeds/SubTableSeeder.php
+++ app/database/seeds/SubTableSeeder.php
@@ -10,7 +10,7 @@ class SubTableSeeder extends Seeder {
 
     for ($i = 0; $i < 10; $i++) {
       Sub::create(array(
-        'name'       => $faker->userName,
+        'name'       => substr(strtr($faker->userName, '.', ''), 0, 20),
         'owner_id'   => $faker->numberBetween($min = 1, $max = 50),
         'created_at' => $faker->unixTime($max = 'now')
       ));
--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -10,7 +10,7 @@ class UserTableSeeder extends Seeder {
 
     for ($i = 0; $i < 50; $i++) {
       User::create(array(
-        'name'       => $faker->userName,
+        'name'       => substr(strtr($faker->userName, '.', ''), 0, 20),
         'password'   => Hash::make($faker->userName),
         'created_at' => $faker->unixTime($max = 'now')
       ));

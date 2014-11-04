6196b381a2be2a1a654aa79c857c57046f4c8433
Nat Zimmermann <nat@natzim.com>
2014-11-04 19:01:28 +0000

    Added comment add method (not working)

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -2,4 +2,24 @@
 
 class CommentController extends BaseController {
 
+  public function create()
+  {
+    $rules = array(
+      'content' => 'required|max:3000'
+    );
+
+    $validator = Validator::make(Input::all(), $rules);
+
+    if ($validator->fails()) {
+      // TODO: Add error
+    } else {
+      Comment::create(array(
+        'post_id'      => Request::segment(2),
+        'user_id'      => Auth::user()->id,
+        // TEMP
+        'parentcmt_id' => 1,
+        'content'      => Input::get('content')
+      ));
+    }
+  }
 }
